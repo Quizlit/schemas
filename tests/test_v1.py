f9292c9fa3306d7e6adffa874eae2cf6a6fde149
Marcus Willock <crazcalm@gmail.com>
2024-06-03 20:27:20 -0400

    fix #2
    I went with the flask file structure (src/repo_name)

---
+++ tests/test_v1.py
@@ -0,0 +1,75 @@
+import unittest
+import json
+from pathlib import Path
+
+from jsonschema import validate, ValidationError
+
+
+class TestBaseClass(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.schema = json.loads(Path("src/schemas/v1/quizlit.json").read_text())
+
+
+class TestV1HappyPath(TestBaseClass):
+    @staticmethod
+    def get_test_cases():
+        return [
+            (
+                "name of test -- example 1",
+                {
+                    "productId": 1,
+                    "productName": "name_1",
+                    "price": 0.01
+                },
+            ),
+            (
+                "name of test -- example 2",
+                {
+                    "productId": 2,
+                    "productName": "name_2",
+                    "price": 1
+                },
+            ),
+        ]
+        
+    def test_v1(self):
+        for name, data in self.get_test_cases():
+            with self.subTest(msg=name):
+                validate(instance=data, schema=self.schema)
+
+
+class TestV1Errors(TestBaseClass):
+    @staticmethod
+    def get_test_cases():
+        return [
+            (
+                "Empty dict",
+                {},
+            ),
+            (
+                "Price is zero",
+                {
+                    "productId": 1,
+                    "productName": "name_1",
+                    "price": 0
+                },
+            ),
+            (
+                "missing ProductName",
+                {
+                    "productId": 2,
+                    "price": 1
+                },
+            ),
+        ]
+        
+    def test_v1(self):
+        for name, data in self.get_test_cases():
+            with self.subTest(msg=name):
+                with self.assertRaises(ValidationError):
+                    validate(instance=data, schema=self.schema)
+
+
+if __name__ == '__main__':
+    unittest.main()
